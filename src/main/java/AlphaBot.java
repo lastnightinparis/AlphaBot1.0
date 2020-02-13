4c9b6dec8b93c71a6d695a7dd81e6be2c3c7e4d7
Kir <tankovskiyko@gmail.com>
2020-02-13 23:45:08 +0300

    test

--- src/main/java/AlphaBot.java
+++ src/main/java/AlphaBot.java
@@ -15,7 +15,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
+//comment
 public class AlphaBot extends TelegramLongPollingBot {
     public String getBotToken() {
         return "1080271817:AAELD57x5s89IVeE-iH3Mof31-zBEoIVhOQ";
